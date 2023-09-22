b4aaf6ba44294e04b41c6ef78b449a1500e356e1
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2023-09-22 17:32:43 +0600

    Add files via upload

---
+++ Be like Hasib.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d%d",&n,&x);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            k++;
+        }
+    }
+    printf("%d\n",k);
+    return 0;
+}
